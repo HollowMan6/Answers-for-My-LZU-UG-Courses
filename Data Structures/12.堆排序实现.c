12ea3d88017b646acb4f2ad77ef832483ed933a7
hollowman6 <hollowman186@vip.qq.com>
2019-12-12 10:46:31 +0800

    Latest push

--- Data Structures/12.堆排序实现.c
+++ Data Structures/12.堆排序实现.c
@@ -2,7 +2,7 @@
 #define MAX 255
 int A[MAX];
 // 建初始堆过程函数
-void creatdui(int l, int m) 
+void heap(int l, int m) 
 {
     int i, j, x;
     i = l;
@@ -28,21 +28,21 @@ void creatdui(int l, int m)
     // 被筛结点的值存入最终的位置
     A[i] = x; 
 }
-void sortdui(int n)
+void heapsort(int n)
 {
     int i;
     int m;
     // 建立初始堆，遍布所有的根结点
     for (i = n / 2; i >= 1; i--)
-        creatdui(i, n); 
-    // 筛选 A[1] 结点，得到 i-1 个结点的堆，因为 A[1] 可能违反堆的性质
+        heap(i, n); 
+    // 筛选 A[1] 结点，得到 i-1 个结点的堆，因为 A[1] 违反堆的性质
     for (i = n; i >= 2; i--)
     { //进行 n-1 次循环完成堆排序
         m = A[i];
         // 将第一个元素同当前区域的最后一个元素对换
         A[i] = A[1]; 
         A[1] = m;
-        creatdui(1, i - 1);
+        heap(1, i - 1);
     } 
 }
 int main()
@@ -65,7 +65,7 @@ int main()
     {
         printf("%d ", A[i]);
     }
-    sortdui(n);
+    heapsort(n);
     printf("\n 堆排序后的序列为 :\n");
     for (i = 1; i <= n; i++)
     {
