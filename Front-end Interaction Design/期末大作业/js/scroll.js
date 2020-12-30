9f947986e65f73133a0986d5d49abad49f7fbc96
Hollow Man <hollowman@hollowman.ml>
2020-12-30 13:19:20 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- Front-end Interaction Design/期末大作业/js/scroll.js
+++ Front-end Interaction Design/期末大作业/js/scroll.js
@@ -2,7 +2,7 @@ ScrollReveal({
     scale: 0.85,
     duration: 1000,
 });
-ScrollReveal().reveal('.top_bottom,.top_nav,.top_logo,.top_tab,.top_middle,.top_next,.top_nav,#container,.nav,canvas', {
+ScrollReveal().reveal('.top_bottom,.top_nav,.top_logo *,.top_tab,.top_middle,.top_next,.top_nav,#container,.nav,canvas,.show *,#content *', {
     delay: 100,
     useDelay: 'onload',
     reset: true,
