44f26705851901fb35dd9f48fcfd7de3c757e43d
Hollow Man <hollowman@hollowman.ml>
2021-03-22 12:34:35 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- Principles of Computer Organization/2021春季学期/ALU32_MIPS/ALU32_MIPS.runs/synth_1/ALU32.tcl
+++ Principles of Computer Organization/2021春季学期/ALU32_MIPS/ALU32_MIPS.runs/synth_1/ALU32.tcl
@@ -70,7 +70,6 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 2
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7k70tfbv676-1
 
--- Principles of Computer Organization/2021春季学期/ALU32_MIPS/ALU32_MIPS.srcs/sim_1/new/ALU_sim.v
+++ Principles of Computer Organization/2021春季学期/ALU32_MIPS/ALU32_MIPS.srcs/sim_1/new/ALU_sim.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Lanzhou University
+// Engineer: Songlin Jiang
 // 
 // Create Date: 15.03.2021 20:30:25
-// Design Name: 
+// Design Name: ALU_32
 // Module Name: ALU_sim
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Project Name: ALU
+// Target Devices: -
+// Tool Versions: Vivado 2020.2
+// Description: -
 // 
-// Dependencies: 
+// Dependencies: -
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: -
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -30,100 +30,114 @@ reg [10:0] op;
     ALU32 alu(op,in0,in1,carryout,overflow,zero,out);
     initial
     begin
-    //add
-         op=11'b00000100000;        
-         in0=32'hf2340000;
-         in1=32'h80000000;
-    #20  in0=32'h7fffffff;
-         in1=32'h70000001;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    #20  in0=32'hffffffff;
-         in1=32'h00000001;
-    //addu          
-    #20  op=11'b00000100001;   
-         in0=32'hf2340000;
-         in1=32'h80000000;
-    #20  in0=32'h7fffffff;
-         in1=32'h70000001;
-    #20  in0=32'hffffffff;
-         in1=32'h00000001;
-    //sub    
-    #20  op=11'b00000100010;        
-         in0=32'h72340000;
-         in1=32'h60000000;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    #20  in0=32'hf00fffff;
-         in1=32'h7ffffff1;
-    #20  in0=32'hffffffff;
-         in1=32'hffffffff;
-    #20  in0=32'hf0000000;
-         in1=32'h0fffffff; 
-    //subu
-    #20  op=11'b00000100011;        
-         in0=32'h72340000;
-         in1=32'h60000000;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    #20  in0=32'hffffffff;
-         in1=32'hffffffff;
-    #20  in0=32'hf0000000;
-         in1=32'h0fffffff; 
-    //and
-    #20  op=11'b00000100100;        
-         in0=32'h72340000;
-         in1=32'h60000000;
-    #20  in0=32'h7fffffff;
-         in1=32'h00000000; 
-    //or
-    #20  op=11'b00000100101;        
-         in0=32'h00000000;
-         in1=32'h00000000;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    //xor
-    #20  op=11'b00000100110;        
-         in0=32'ha0000000;
-         in1=32'h50000000;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    //nor
-    #20  op=11'b00000100111;        
-         in0=32'h123451ff;
-         in1=32'h60000000;
-    #20  in0=32'h7fffffff;
-         in1=32'hf0000001;
-    //slt
-    #20  op=11'b00000101010;        
-         in0=32'h72340000;
-         in1=32'hf0000000;
-    #20  in0=32'h7000000f;
-         in1=32'h7f000001;
-    #20  in0=32'hf0001231;
-         in1=32'h7ac34545;
-    //sltu
-    #20  op=11'b00000101011;        
-         in0=32'h72340000;
-         in1=32'hf0000000;
-    #20  in0=32'h7000000f;
-         in1=32'h7f000001;
-    #20  in0=32'hf0001231;
-         in1=32'h7ac34545;
-    //shl
-    #20  op=11'b00000000100;
-         in0=32'hffffffff;
-         in1=32'd5;
-    //shr
-    #20  op=11'b00000000110;
-         in0=32'hffffffff;
-         in1=32'd5;
-    //sar
-    #20  op=11'b00000000111;
-         in0=32'hffffffff;
-         in1=32'd3;
-    #20  in0=32'h0fffffff;
-         in1=32'd5;
+     //add
+          op=11'b00000100000;        
+          in0=32'h7fffffff;
+          in1=32'h70000001;
+     #20  in0=32'h2fffffff;
+          in1=32'hf0000001;
+     #20  in0=32'hffffffff;
+          in1=32'h00000001;
+     #20  in0=32'hf2340000;
+          in1=32'h90000000;
+     //addu          
+     #20  op=11'b00000100001;   
+          in0=32'hf2340000;
+          in1=32'h80000000;
+     #20  in0=32'h7fffffff;
+          in1=32'h70000001;
+     #20  in0=32'hffffffff;
+          in1=32'h00000001;
+     //sub    
+     #20  op=11'b00000100010;        
+          in0=32'h72340000;
+          in1=32'h60000000;
+     #20  in0=32'h7fffffff;
+          in1=32'hf0000001;
+     #20  in0=32'hf00fffff;
+          in1=32'h7ffffff1;
+     #20  in0=32'hffffffff;
+          in1=32'hffffffff;
+     #20  in0=32'hf0000000;
+          in1=32'h0fffffff; 
+     //subu
+     #20  op=11'b00000100011;        
+          in0=32'h72340000;
+          in1=32'h60000000;
+     #20  in0=32'h7fffffff;
+          in1=32'hf0000001;
+     #20  in0=32'hffffffff;
+          in1=32'hffffffff;
+     #20  in0=32'hf0000000;
+          in1=32'h0fffffff; 
+     //and
+     #20  op=11'b00000100100;        
+          in0=32'h72340000;
+          in1=32'h60000000;
+     #20  in0=32'h7fffffff;
+          in1=32'h00000000; 
+     //or
+     #20  op=11'b00000100101;        
+          in0=32'h00000000;
+          in1=32'h00000000;
+     #20  in0=32'h7fffffff;
+          in1=32'hf0000001;
+     //xor
+     #20  op=11'b00000100110;        
+          in0=32'ha0000000;
+          in1=32'h50000000;
+     #20  in0=32'h7fffffff;
+          in1=32'hf0000001;
+     //nor
+     #20  op=11'b00000100111;        
+          in0=32'h123451ff;
+          in1=32'h60000000;
+     #20  in0=32'h7fffffff;
+          in1=32'hf0000001;
+     //slt
+     #20  op=11'b00000101010;        
+          in0=32'h72340000;
+          in1=32'hf0000000;
+     #20  in0=32'h7000000f;
+          in1=32'h7f000001;
+     #20  in0=32'hf0001231;
+          in1=32'h7ac34545;
+     //sltu
+     #20  op=11'b00000101011;        
+          in0=32'h72340000;
+          in1=32'hf0000000;
+     #20  in0=32'h7000000f;
+          in1=32'h7f000001;
+     #20  in0=32'hf0001231;
+          in1=32'h7ac34545;
+     //sll
+     #20  op=11'b00000000000;
+          in0=32'hffffffff;
+          in1=32'd0;
+     //srl
+     #20  op=11'b00000000010;
+          in0=32'hffffffff;
+          in1=32'd1;
+     //sar
+     #20  op=11'b00000000011;
+          in0=32'hffffffff;
+          in1=32'd2;
+     #20  in0=32'h0fffffff;
+          in1=32'd3;
+     //sllv
+     #20  op=11'b00000000100;
+          in0=32'hffffffff;
+          in1=32'd5;
+     //srlv
+     #20  op=11'b00000000110;
+          in0=32'hffffffff;
+          in1=32'd5;
+     //sarv
+     #20  op=11'b00000000111;
+          in0=32'hffffffff;
+          in1=32'd3;
+     #20  in0=32'h0fffffff;
+          in1=32'd5;
     end
     
 endmodule
